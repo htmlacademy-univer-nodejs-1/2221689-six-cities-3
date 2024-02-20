db8f30a2e351074b397af5a220e3627d408b3da2
Viktoria <Viktoria.Khmeleva@urfu.me>
2024-02-21 03:20:32 +0500

    Приложение для терминала

---
+++ src/shared/types/rental-offer.type.ts
@@ -0,0 +1,23 @@
+import { Convenience } from "./convenience.enum.js";
+import { HousingType } from "./housing-type.enum.js";
+import { User } from "./user.type.js";
+
+export type RentalOffer = {
+   title: string;
+   description: string;
+   createdDate: Date;
+   city: string;
+   previewImage: string;
+   images: string[];
+   premium: boolean;
+   favorite: boolean;
+   rating: number;
+   type: HousingType;
+   roomsCount: number;
+   guestsCount: number;
+   price: number;
+   conveniences: Convenience[];
+   authorOffer: User;
+   commentsCount: number;
+   coordinats: [string, string]
+}
